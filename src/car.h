dffc408927b9eee97636c630d9e06a3122bb9a65
trort <luojiajun432100@gmail.com>
2018-07-16 01:42:25 -0400

    A modulerized version that tries to change to the fastest lane.

---
+++ src/car.h
@@ -0,0 +1,8 @@
+//
+// Created by Jiajun Luo on 7/16/18.
+//
+
+#ifndef PATH_PLANNING_CAR_H
+#define PATH_PLANNING_CAR_H
+
+#endif //PATH_PLANNING_CAR_H
---
+++ src/helper_functions.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Jiajun Luo on 7/16/18.
+//
+
