a626fc4979aed25beac53a1a4bbca49c6c2b07a3
trort <luojiajun432100@gmail.com>
2018-07-24 00:24:26 -0400

    A working version that is able to change lane and avoid collision.

--- src/car.h
+++ src/car.h
@@ -21,6 +21,7 @@ public:
 
   string state;
   int lane;
+  int end_lane;
   int target_lane;
 
   // Main car's localization Data
@@ -52,10 +53,14 @@ public:
   vector<double> map_waypoints_dx;
   vector<double> map_waypoints_dy;
 
+  // cached history of target velocity in each lane
+  vector<double> ref_vel;
+
   Car(){
     state = "KL";
     lane = 1;
     target_lane = -1;
+    ref_vel = {MAX_VEL, MAX_VEL, MAX_VEL};
   }
   ~Car(){}
 
@@ -66,12 +71,10 @@ public:
     car_d = j["d"];
     car_yaw = deg2rad(j["yaw"]);
     car_speed = double(j["speed"]) / 2.24;
-    end_s = j["end_path_s"];
-    end_d = j["end_path_d"];
     lane = d2lane(car_d);
     if (target_lane == -1) target_lane = lane;
 
-    prev_size = min(int(j["previous_path_x"].size()), 40);    // load no more than 40 points (0.8s) to allow path innovation
+    prev_size = min(int(j["previous_path_x"].size()), 10);    // load no more than 40 points (0.8s) to allow path innovation
     previous_path_x.clear(); previous_path_y.clear();
     for (int i = 0; i < prev_size; ++i) {
       previous_path_x.push_back(j["previous_path_x"][i]);
@@ -95,7 +98,12 @@ public:
       end_y_prev = previous_path_y[prev_size-2];
       end_yaw = atan2(end_y - end_y_prev, end_x - end_x_prev);
       end_speed = distance(end_x, end_y, end_x_prev, end_y_prev) / INTERVAL;
+      vector<double> end_sd = getFrenet(end_x, end_y, end_yaw, map_waypoints_x, map_waypoints_y);
+      end_s = end_sd[0];
+      end_d = end_sd[1];
+      end_lane = d2lane(end_d);
     }
+
     print_car_state();
   }
 
@@ -110,36 +118,73 @@ public:
   void print_car_state(){
     cout << "Car location: x="<< car_x << "; y=" << car_y << "; s=" << car_s << "; d=" << car_d
          << "; speed=" << car_speed << "; yaw=" << car_yaw << endl;
-    //cout << "End location: x="<< end_x << "; y=" << end_y << "; s=" << end_s << "; d=" << end_d
-    //     << "; speed=" << end_speed << "; yaw=" << end_yaw << endl;
+    cout << "End location: x="<< end_x << "; y=" << end_y << "; s=" << end_s << "; d=" << end_d
+         << "; speed=" << end_speed << "; yaw=" << end_yaw << endl;
   }
 
   vector<string> get_next_states(){
     vector<string> next_states;
-    if (lane == target_lane) {     // not during lane change
+    if (fabs(end_d - (target_lane * 4.0 + 2.0)) > 0.2) {      // keep finish lane change
+      next_states.push_back("KL");
+    }
+    else {     // not during lane change
       next_states.push_back("KL");
       if (state.compare("KL") == 0) {
-        if (lane > 0) next_states.push_back("PLCL");
-        if (lane < 2) next_states.push_back("PLCR");
+        if (end_lane > 0) next_states.push_back("PLCL");
+        if (end_lane < 2) next_states.push_back("PLCR");
       } else if (state.compare("PLCL") == 0) {
-        if (lane > 0) {
+        if (end_lane > 0) {
           next_states.push_back("PLCL");
           next_states.push_back("LCL");
         }
       } else if (state.compare("PLCR") == 0) {
-        if (lane < 2) {
+        if (end_lane < 2) {
           next_states.push_back("PLCR");
           next_states.push_back("LCR");
         }
       }
     }
-    else {      // keep finish lane change
-      next_states.push_back(state);
-    }
+
     return next_states;
   }
 
-  vector<vector<double>> generate_trajectory(string next_state, vector<double>& ref_vel){
+  void update_lane_vel(json& sensor_fusion){
+    vector<double> new_vel = {MAX_VEL, MAX_VEL, MAX_VEL};
+
+    // sensor fusion analysis
+    for (int check_lane = 0; check_lane < 3; ++ check_lane) {
+      for (int i = 0; i < sensor_fusion.size(); ++i) {
+        // unpack sensor fusion data
+        int ss_id = sensor_fusion[i][0];
+        double ss_x = sensor_fusion[i][1];
+        double ss_y = sensor_fusion[i][2];
+        double ss_vx = sensor_fusion[i][3];
+        double ss_vy = sensor_fusion[i][4];
+        double ss_s = sensor_fusion[i][5];
+        double ss_d = sensor_fusion[i][6];
+
+        if (ss_d < (2 + 4 * check_lane + 2) && ss_d > (2 + 4 * check_lane - 2)) { // same lane
+          double ss_speed = sqrt(ss_vx * ss_vx + ss_vy * ss_vy);
+          double check_car_s = ss_s + prev_size * INTERVAL * ss_speed;
+
+          if (check_lane == lane) {
+            if ((ss_s >= car_s) && ((check_car_s - end_s) < 40)) {
+              new_vel[check_lane] = min(new_vel[check_lane], ss_speed - 0.2);
+            }
+          }
+          else {
+            if (((ss_s >= car_s) || (check_car_s >= end_s)) && ((check_car_s - end_s) < 40)) {
+              new_vel[check_lane] = min(new_vel[check_lane], ss_speed - 0.2);
+            }
+          }
+        }
+      }
+      ref_vel[check_lane] = 0.98 * ref_vel[check_lane] + 0.02 * new_vel[check_lane];
+    }
+    cout << "Lane speed: " << ref_vel[0] << " | " << ref_vel[1] << " | " << ref_vel[2] << endl;
+  }
+
+  vector<vector<double>> generate_trajectory(string next_state){
     vector<double> next_x_vals;
     vector<double> next_y_vals;
 
@@ -152,24 +197,24 @@ public:
     ptsy.push_back(end_y_prev);
     ptsy.push_back(end_y);
 
-    int target_lane = lane + lane_delta.at(next_state);
-    double target_vel = ref_vel[lane];
+    int new_target_lane = target_lane + lane_delta.at(next_state);
+    double target_vel = ref_vel[target_lane];
     if (next_state.compare("PLCL") == 0 || next_state.compare("PLCR") == 0) {
-      target_vel = min(ref_vel[target_lane], target_vel);
+      target_vel = min(ref_vel[new_target_lane], target_vel);
     }
     else if (next_state.compare("LCL") == 0 || next_state.compare("LCR") == 0){
-      target_vel = ref_vel[target_lane];
+      target_vel = ref_vel[new_target_lane];
     }
-    int next_lane = lane;
+    int next_lane = target_lane;
     if (next_state.compare("LCL") == 0 || next_state.compare("LCR") == 0) {
-      next_lane = target_lane;
+      next_lane = new_target_lane;
     }
     double next_d = 2 + 4 * next_lane;
     vector<double> next_wp0 = getXY(end_s + 30, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
     ptsx.push_back(next_wp0[0]); ptsy.push_back(next_wp0[1]);
-    vector<double> next_wp1 = getXY(end_s + 60, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+    vector<double> next_wp1 = getXY(end_s + 50, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
     ptsx.push_back(next_wp1[0]); ptsy.push_back(next_wp1[1]);
-    vector<double> next_wp2 = getXY(end_s + 90, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+    vector<double> next_wp2 = getXY(end_s + 70, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
     ptsx.push_back(next_wp2[0]); ptsy.push_back(next_wp2[1]);
 
     // transform to car coordinates and the end to ensure going forward
@@ -188,14 +233,14 @@ public:
     }
 
     // fine-spaced points
-    double target_x = 30;
+    double target_x = 50;
     double target_y = s(target_x);
     double target_dist = distance(0, 0, target_x, target_y);
     double x_add_on = 0;
 
     double car_vel = end_speed;
 
-    for (int i = 0; i < 80 - prev_size; ++i) {
+    for (int i = 0; i < 70 - prev_size; ++i) {
 
       if (car_vel > target_vel) {
         car_vel -= 0.1;
@@ -218,57 +263,18 @@ public:
     return {next_x_vals, next_y_vals};
   }
 
-  vector<double> get_lane_vel(json& sensor_fusion){
-    vector<double> ref_vel = {MAX_VEL, MAX_VEL, MAX_VEL};
-
-    // sensor fusion analysis
-    for (int check_lane = 0; check_lane < 3; ++ check_lane) {
-      for (int i = 0; i < sensor_fusion.size(); ++i) {
-        // unpack sensor fusion data
-        int ss_id = sensor_fusion[i][0];
-        double ss_x = sensor_fusion[i][1];
-        double ss_y = sensor_fusion[i][2];
-        double ss_vx = sensor_fusion[i][3];
-        double ss_vy = sensor_fusion[i][4];
-        double ss_s = sensor_fusion[i][5];
-        double ss_d = sensor_fusion[i][6];
-
-        if (ss_d < (2 + 4 * check_lane + 2) && ss_d > (2 + 4 * check_lane - 2)) { // same lane
-          double ss_speed = sqrt(ss_vx * ss_vx + ss_vy * ss_vy);
-          double check_car_s = ss_s + prev_size * INTERVAL * ss_speed;
-
-          if ((check_car_s >= end_s) && ((check_car_s - end_s) < 30)) {
-            ref_vel[check_lane] = min(ref_vel[check_lane], ss_speed);
-          }
-        }
-      }
-    }
-    return ref_vel;
-  }
-
   vector<vector<double>> generate_path(json& j){
     update_car(j);
-    vector<double> ref_vel = get_lane_vel(j["sensor_fusion"]);
-    cout << "Lane speed: " << ref_vel[0] << " | " << ref_vel[1] << " | " << ref_vel[2] << endl;
+    update_lane_vel(j["sensor_fusion"]);
 
     vector<string> next_states = get_next_states();
     vector<vector<vector<double>>> next_trajectories;
     vector<double> next_costs;
     for(int i = 0; i < next_states.size(); ++i) {
       string next_state = next_states[i];
-      vector<vector<double>> next_trajectory = generate_trajectory(next_state, ref_vel);
+      vector<vector<double>> next_trajectory = generate_trajectory(next_state);
       next_trajectories.push_back(next_trajectory);
       cout << "Cost for " << next_state << " is:";
-      /*vector<double> next_trajectory_s;
-      vector<double> next_trajectory_d;
-      for (int time_i = 0; time_i < next_trajectory[0].size(); time_i ++){
-        vector<double> next_sd = getFrenet(next_trajectory[0][time_i], next_trajectory[1][time_i], car_yaw, map_waypoints_x, map_waypoints_y);
-        cout << next_trajectory[0][time_i] << "," << next_trajectory[1][time_i] << " ";
-        next_trajectory_s.push_back(next_sd[0]);
-        next_trajectory_d.push_back(next_sd[1]);
-      }
-      cout << endl;
-      vector<vector<double>> next_trajectory_sd = {next_trajectory_s, next_trajectory_d};*/
       double next_cost = total_cost(next_trajectory, next_state, INTERVAL, MAX_VEL, j["sensor_fusion"],
                                     map_waypoints_s, map_waypoints_x, map_waypoints_y);
       next_costs.push_back(next_cost);
@@ -282,10 +288,12 @@ public:
         best_cost = next_costs[i];
       }
     }
-    state = next_states[best_next_idx];
-    if (state.compare("LCL") == 0 || state.compare("LCR") == 0){
-      target_lane = lane + lane_delta.at(state);
+    string new_state = next_states[best_next_idx];
+    if ((new_state.compare("LCL") == 0 || new_state.compare("LCR") == 0) && new_state.compare(state) != 0){
+      target_lane = target_lane + lane_delta.at(new_state);
+      cout << j << endl;
     }
+    state = new_state;
     cout << "Selected state: " << state << "; current lane = " << lane << "; target lane = " << target_lane << endl;
 
     return next_trajectories[best_next_idx];
--- src/cost.h
+++ src/cost.h
@@ -13,14 +13,25 @@ double collision_cost(vector<double>& path_x, vector<double>& path_y, const doub
                       const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y){
   double cost = 0;
   const double s_tolerance = 10;
-  const double d_tolerance = 3.2;
-  if (path_x.size() > 1 && sensor_fusion.size() >0) {
+  const double d_tolerance = 2.6;
+  if (path_x.size() > 1 && sensor_fusion.size() > 0) {
+    double start_x = path_x[0];
+    double start_y = path_y[0];
+    double start_yaw = atan2(path_y[1] - path_y[0], path_x[1] - path_x[0]);
+    vector<double> start_sd = getFrenet(start_x, start_y, start_yaw, maps_x, maps_y);
+    double start_s = start_sd[0];
+    double start_d = start_sd[1];
+    int start_lane = d2lane(start_d);
     for (int time_i = 1; time_i < path_x.size(); ++time_i) {
       double ego_x = path_x[time_i];
       double ego_y = path_y[time_i];
-      double ego_yaw = atan2(path_y[time_i] - path_y[time_i - 1], path_x[time_i] - path_x[time_i]);
+      double ego_yaw = atan2(path_y[time_i] - path_y[time_i - 1], path_x[time_i] - path_x[time_i - 1]);
+      vector<double> ego_sd = getFrenet(ego_x, ego_y, ego_yaw, maps_x, maps_y);
+      double ego_s = ego_sd[0];
+      double ego_d = ego_sd[1];
 
       for (int ss_i = 0; ss_i < sensor_fusion.size(); ++ ss_i) {
+        int ss_id = sensor_fusion[ss_i][0];
         double ss_x = sensor_fusion[ss_i][1];
         double ss_y = sensor_fusion[ss_i][2];
         double ss_vx = sensor_fusion[ss_i][3];
@@ -28,18 +39,22 @@ double collision_cost(vector<double>& path_x, vector<double>& path_y, const doub
         double ss_s = sensor_fusion[ss_i][5];
         double ss_d = sensor_fusion[ss_i][6];
         double ss_speed = sqrt(ss_vx * ss_vx + ss_vy * ss_vy);
+        int ss_lane = d2lane(ss_d);
+        if (ss_lane == start_lane && ss_s < start_s - 10) continue; // assume car behind will not try to hit us
 
-        ss_s += ss_speed * time_i * dt;
-        ss_d += 0;
+        double ss_end_s = ss_s + ss_speed * time_i * dt;
+        double ss_end_d = ss_d + 0;
 
-        vector<double> ss_xy = getXY(ss_s, ss_d, maps_s, maps_x, maps_y);
-        vector<double> car_coord_sd = map_xy_to_ref(ss_xy[0], ss_xy[1], ego_x, ego_y, ego_yaw);
-
-        if (abs(car_coord_sd[0]) < s_tolerance && abs(car_coord_sd[1]) < d_tolerance) {
-          cost += (s_tolerance / abs(car_coord_sd[0]) + d_tolerance / abs(car_coord_sd[1]));
+        if (fabs(ego_s - ss_end_s) < 5 * s_tolerance && fabs(ego_d - ss_end_d) < d_tolerance) {
+          double new_cost = pow(s_tolerance / fabs(ego_s - ss_end_s), 3.0);// + pow(d_tolerance / fabs(ego_d - ss_end_d), 2.0);
+          cost = max(cost, new_cost);
+          /*cout << "COLLISION: t=" << time_i << ", " << ego_s << "," << ego_d << " with "
+               << "car " << ss_id << " " << ss_end_s << "," << ss_end_d
+               << " current " << ss_s << "," << ss_d << " cost=" << new_cost << endl;*/
         }
       }
     }
+    // cost /= (path_x.size() - 1);
   }
   return cost;
 }
@@ -80,7 +95,7 @@ double acceleration_cost(vector<double>& path_x, vector<double>& path_y, const d
 }
 
 double lane_change_cost(string state){
-  if (state.compare("LCL") == 0 || state.compare("LCR") == 0) return 1;  // try limit lane change
+  if (state.compare("LCL") == 0 || state.compare("LCR") == 0) return 10;  // try limit lane change
   else if (state.compare("PLCL") == 0 || state.compare("PLCR") == 0) return -0.1; // be more prepared
   else return 0;
 }
@@ -92,11 +107,11 @@ double total_cost(vector<vector<double>>& trajectory, string& state, const doubl
   double acceleration_term = acceleration_cost(trajectory[0], trajectory[1], dt);
   double lane_change_term = lane_change_cost(state);
   double cost = 0;
-  cost += 500 * collision_term;
+  cost += 10 * collision_term;
   cost += 1 * speed_term;
-  // cost += 1 * acceleration_term;
+  cost += 0.001 * acceleration_term;
   cost += 1 * lane_change_term;
-  cout << " total=" << cost << "; collision=" << collision_term << "; speed=" << speed_term
+  cout << "total=" << cost << "; collision=" << collision_term << "; speed=" << speed_term
        << "; acc=" << acceleration_term << endl;
   return cost;
 }
